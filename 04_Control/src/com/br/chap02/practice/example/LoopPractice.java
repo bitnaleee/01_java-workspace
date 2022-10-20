cfc620aec3a4fd320d307a7e3a6b2c668afe8ca7
bitna_lee <lbn063017@naver.com>
2022-10-20 17:46:34 +0900

    221020 반복문 for문

---
+++ 04_Control/src/com/br/chap02/practice/example/LoopPractice.java
@@ -0,0 +1,155 @@
+package com.br.chap02.practice.example;
+
+import java.util.Scanner;
+
+public class LoopPractice {
+	
+	public void practice1() {
+		
+		Scanner sc =  new Scanner(System.in);
+		System.out.print("1이상의 숫자를 입력하세요 : ");
+		int num = sc.nextInt();
+		
+		if(num>=1) {
+			
+			for(int i=1; i<=num; i++) {
+				System.out.print(i + " ");
+			}
+			
+		} else {
+			System.out.println("잘못 입력하셨습니다");
+		}
+		
+	}
+	
+	public void practice2() {
+		
+		Scanner sc =  new Scanner(System.in);
+		System.out.print("1이상의 숫자를 입력하세요 : ");
+		int num = sc.nextInt();
+		
+		if(num>=1) {
+			
+			for(int i=1; i<=num; i++) {
+				System.out.print(i + " ");
+			} return;
+			
+		} else {
+			System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
+		}
+
+	}
+	
+	public void practice3() {
+		/*
+		Scanner sc =  new Scanner(System.in);
+		System.out.print("1이상의 숫자를 입력하세요 : ");
+		int num = sc.nextInt();
+		
+		if(num>=1) {
+			
+			for(int i=1; i<=num; i--) {
+				System.out.print(i + " ");
+			}
+			
+		} else {
+			System.out.println("잘못 입력하셨습니다.");
+		}
+		
+	}
+	
+	public void practice5() {
+		
+		Scanner sc =  new Scanner(System.in);
+		System.out.print("정수를 하나 입력하세요 : ");
+		int num = sc.nextInt();
+		
+		for(int i=1; i<=num; i++) {
+		
+		} 
+		
+		*/
+	}
+	
+	public void practice6() {
+		
+		/*
+		Scanner sc =  new Scanner(System.in);
+		System.out.print("첫 번째 숫자 : ");
+		int num = sc.nextInt();
+		System.out.print("두 번째 숫자 : ");
+		int num2 = sc.nextInt();
+		
+		if(num >= 1 ) {
+			
+			if(num < num2) {
+				for(int i=num; i<=num2; i++) {
+					System.out.print(i + " ");
+				}	
+			} else {
+				for(int i=num2; i<=num; i++) {
+					System.out.print(i + " ");
+			}
+			
+		} else {
+			System.out.print("1이상의 숫자만을 입력해주세요");
+		}
+		*/
+		
+	}
+	
+	public void practice8() {
+		
+		Scanner sc =  new Scanner(System.in);
+		System.out.print("숫자 : ");
+		int num = sc.nextInt();
+		
+		if(num >= 1 && num <= 9) {
+	
+			for(int i=1; i<=9; i++) {
+				System.out.printf("%d * %d = %d\n", num, i, num*i);
+			}
+			
+		} else {
+			System.out.println("다시 입력해주세요");
+		}
+		
+	}
+	
+	public void practice9() {
+		/*
+		Scanner sc =  new Scanner(System.in);
+		System.out.print("숫자 : ");
+		int num = sc.nextInt();
+		
+		if(num >= 1 && num <= 9) {
+			
+			for (int j=num; j<=9; j++) > num~9단까지 반복
+			
+			for(int num=2; num<=9; num++) {
+				System.out.println("=== " + num + "단 ===");  > num 이름 출력 
+				for(int i=1; i<=9; i++) {
+					System.out.printf("%d * %d = %d\n", num, i, num*i); > num 구구단 X i
+			}  
+				
+			}
+			
+		} else {
+			System.out.println("2~9사이의 숫자만 입력해주세요");
+		}
+		*/
+		
+	}
+	
+	public void practice11() {
+		
+		Scanner sc =  new Scanner(System.in);
+		System.out.print("시작 숫자 : ");
+		int num1 = sc.nextInt();
+		System.out.print("공차 : ");
+		int num2 = sc.nextInt();
+		
+		
+	}
+	
+}
---
+++ 04_Control/src/com/br/chap02/run/LoopRun.java
@@ -0,0 +1,33 @@
+package com.br.chap02.run;
+
+import com.br.chap02.loop.*;
+
+public class LoopRun {
+	
+	public static void main(String[] args) {
+		
+		A_For f = new A_For();
+		//f.method1();
+		//f.method2();
+		//f.method3();
+		//f.method4();
+		//f.method5();
+		//f.method6();
+		//f.method7();
+		//f.method8();
+		//f.method9();
+		//f.method10();
+		//f.method11();
+		//f.method12();
+		//f.method13();
+		//f.method14();
+		//f.method15();
+		//f.method16();
+		f.method17();
+		
+		
+		
+	}
+	
+
+}
