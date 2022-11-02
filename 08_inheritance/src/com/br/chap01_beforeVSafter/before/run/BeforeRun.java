72e50972a96545c0f430d265f4d226f82952d455
bitna_lee <lbn063017@naver.com>
2022-11-02 17:46:39 +0900

    221102 수업

---
+++ 08_inheritance/src/com/br/chap01_beforeVSafter/before/run/BeforeRun.java
@@ -0,0 +1,42 @@
+package com.br.chap01_beforeVSafter.before.run;
+
+import com.br.chap01_beforeVSafter.before.model.vo.Desktop;
+import com.br.chap01_beforeVSafter.before.model.vo.SmartPhone;
+import com.br.chap01_beforeVSafter.before.model.vo.Tv;
+
+public class BeforeRun {
+
+	public static void main(String[] args) {
+		
+		// Desktop 객체 생성
+		// brand, pCode, pName, price, allInOne
+		
+		Desktop d = new Desktop("삼성", "d-01", "짱짱데스크탑", 2000000, true);
+		
+		// Tv 객체 생성
+		// brand, pCode, pName, price, inch
+		
+		Tv t = new Tv("엘지", "t-01", "겁나얉은티비", 3500000, 65);
+		
+		// SmartPhone 객체 생성
+		// brand, pCode, pName, price, mobileAgency
+		
+		SmartPhone s = new SmartPhone("애플", "s-01", "아이뽄", 1300000, "KT");
+		
+		System.out.println(d.information());
+		System.out.println(t.information());
+		System.out.println(s.information());
+		
+		/*
+		 * 세 클래스에 공통적인 필드와 공통적인 메소드들 존재
+		 * 중복된 코드들을 따로 "부모" 클래스로 한 번만 정의해두면 중복된 코드를 줄일 수 있음
+		 * 
+		 * => brand, pCode, pName, price 필드들, setter/getter메소드, information
+		 * => product클래스로 정의 
+		 */
+		
+		
+		
+	}
+	
+}
---
+++ 08_inheritance/src/com/br/chap02_inherit/model/vo/Car.java
@@ -0,0 +1,33 @@
+package com.br.chap02_inherit.model.vo;
+
+public class Car extends Vehicle{
+	
+	private int tire;
+	
+	public Car() {
+		// super(); 보이지 않지만 존재함. 부모생성자(기본생성자)호출하고 있음
+	}
+
+	public Car(String name, double mileage, String kind, int tire) {
+		super(name, mileage, kind);
+		this.tire = tire;
+	}
+	
+	public int getTire() {
+		return tire;
+	}
+	
+	public void setTire(int tire) {
+		this.tire = tire;
+	}
+	// 오버라이딩
+	public String information() {
+		return super.information() + ", tire : " + tire;
+	}
+	
+	public int hashCode() {
+		return 1;
+	}
+	
+	
+}
